9afbd9aac6643be26ab58909270c9d3b21305505
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-05-07 23:49:44 +0300

    Implement validation and delegate

--- Source/TrickyUserInterface/Private/StetingsMenu/SliderWidget.cpp
+++ Source/TrickyUserInterface/Private/StetingsMenu/SliderWidget.cpp
@@ -10,12 +10,41 @@ void USliderWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
 
-	Slider_Main->OnValueChanged.AddDynamic(this, &USliderWidget::HandleValueChange);
-	
+	if (IsValid(Slider_Main))
+	{
+		Slider_Main->OnValueChanged.AddDynamic(this, &USliderWidget::ChangeValue);
+	}
+}
+
+void USliderWidget::ChangeValue(const float Value)
+{
+	OnValueChanged.Broadcast(Value);
+	SetTextValue(Value);
+	HandleValueChange(Value);
 }
 
 void USliderWidget::HandleValueChange_Implementation(const float Value)
 {
+}
+
+void USliderWidget::SetValue(const float Value)
+{
+	if (!IsValid(Slider_Main))
+	{
+		return;
+	}
+	
+	Slider_Main->SetValue(Value);
+	SetTextValue(Value);
+}
+
+void USliderWidget::SetTextValue(const float Value) const
+{
+	if (!IsValid(Text_Value))
+	{
+		return;
+	}
+	
 	const FString Text = FString::Printf(TEXT("%.2f"), Value);
 	Text_Value->SetText(FText::FromString(Text));
 }
--- Source/TrickyUserInterface/Public/StetingsMenu/SliderWidget.h
+++ Source/TrickyUserInterface/Public/StetingsMenu/SliderWidget.h
@@ -4,9 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "BaseUserWidget.h"
+#include "Components/Slider.h"
 #include "SliderWidget.generated.h"
 
-class USlider;
 class UTextBlock;
 
 /**
@@ -19,15 +19,28 @@ class TRICKYUSERINTERFACE_API USliderWidget : public UBaseUserWidget
 
 protected:
 	virtual void NativeConstruct() override;
-	
+
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Slider", meta=(BindWidget))
 	TObjectPtr<USlider> Slider_Main = nullptr;
 	
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Slider", meta=(BindWidget))
 	TObjectPtr<UTextBlock> Text_Value = nullptr;
 
+	UFUNCTION()
+	void ChangeValue(const float Value);
+
 	UFUNCTION(BlueprintNativeEvent, Category="Slider")
 	void HandleValueChange(const float Value);
 
 	virtual void HandleValueChange_Implementation(const float Value);
+	
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnFloatValueChangedEvent OnValueChanged;
+
+	UFUNCTION(BlueprintCallable, Category="Slider")
+	void SetValue(const float Value);
+
+private:
+	void SetTextValue(const float Value) const;
 };
