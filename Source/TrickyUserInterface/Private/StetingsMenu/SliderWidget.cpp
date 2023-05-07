bd874f9e4e5d451ae78a45dfb13a117f3a00d0ec
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-05-07 23:13:57 +0300

    Create basic slider widget

---
+++ Source/TrickyUserInterface/Private/StetingsMenu/SliderWidget.cpp
@@ -0,0 +1,21 @@
+﻿// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+
+#include "StetingsMenu/SliderWidget.h"
+
+#include "Components/Slider.h"
+#include "Components/TextBlock.h"
+
+void USliderWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	Slider_Main->OnValueChanged.AddDynamic(this, &USliderWidget::HandleValueChange);
+	
+}
+
+void USliderWidget::HandleValueChange_Implementation(const float Value)
+{
+	const FString Text = FString::Printf(TEXT("%.2f"), Value);
+	Text_Value->SetText(FText::FromString(Text));
+}
---
+++ Source/TrickyUserInterface/Public/StetingsMenu/SliderWidget.h
@@ -0,0 +1,33 @@
+﻿// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseUserWidget.h"
+#include "SliderWidget.generated.h"
+
+class USlider;
+class UTextBlock;
+
+/**
+ * 
+ */
+UCLASS()
+class TRICKYUSERINTERFACE_API USliderWidget : public UBaseUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeConstruct() override;
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Slider", meta=(BindWidget))
+	TObjectPtr<USlider> Slider_Main = nullptr;
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Slider", meta=(BindWidget))
+	TObjectPtr<UTextBlock> Text_Value = nullptr;
+
+	UFUNCTION(BlueprintNativeEvent, Category="Slider")
+	void HandleValueChange(const float Value);
+
+	virtual void HandleValueChange_Implementation(const float Value);
+};
