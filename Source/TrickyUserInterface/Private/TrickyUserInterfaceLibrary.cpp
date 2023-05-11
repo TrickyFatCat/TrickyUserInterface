b534fff6ad27b2169468d9245ea3131bb213ad7a
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-05-11 21:35:45 +0300

    Add conversion function

--- Source/TrickyUserInterface/Private/TrickyUserInterfaceLibrary.cpp
+++ Source/TrickyUserInterface/Private/TrickyUserInterfaceLibrary.cpp
@@ -4,6 +4,7 @@
 #include "TrickyUserInterfaceLibrary.h"
 
 #include "GameFramework/GameUserSettings.h"
+#include "Kismet/KismetMathLibrary.h"
 
 void UTrickyUserInterfaceLibrary::SetQualitySettings(const int32 QualityIndex)
 {
@@ -57,4 +58,56 @@ void UTrickyUserInterfaceLibrary::SetResolutionScale(const float Scale)
 
 	UserSettings->SetResolutionScaleNormalized(Scale);
 	UserSettings->ApplySettings(false);
+}
+
+FString UTrickyUserInterfaceLibrary::ConvertTimeSeconds(const float TimeSeconds, const ETimeFormat TimeFormat)
+{
+	const FTimespan Timespan = UKismetMathLibrary::FromSeconds(TimeSeconds);
+
+	FString Result = "";
+
+	const int32 TotalMinutes = static_cast<int32>(Timespan.GetTotalMinutes());
+	const int32 Seconds = Timespan.GetSeconds();
+	const int32 TotalSeconds = (FMath::CeilToInt(Timespan.GetTotalSeconds()));
+	const int32 Milliseconds = Timespan.GetFractionMilli();
+
+	auto ConvertMilliseconds = [&Milliseconds](const float Fraction) -> int32
+	{
+		return static_cast<int32>(Milliseconds * Fraction);
+	};
+
+	switch (TimeFormat)
+	{
+	case ETimeFormat::MM_SS_MsMs:
+		Result = FString::Printf(TEXT("%02d:%02d.%02d"),
+		                         TotalMinutes,
+		                         Seconds,
+		                         ConvertMilliseconds(0.1f));
+		break;
+
+	case ETimeFormat::MM_SS_Ms:
+		Result = FString::Printf(TEXT("%02d:%02d.%d"),
+		                         TotalMinutes,
+		                         Seconds,
+		                         ConvertMilliseconds(0.01f));
+		break;
+
+	case ETimeFormat::MM_SS:
+		Result = FString::Printf(TEXT("%02d:%02d"), TotalMinutes, Seconds);
+		break;
+
+	case ETimeFormat::SS_MsMs:
+		Result = FString::Printf(TEXT("%02d.%02d"), TotalSeconds, ConvertMilliseconds(0.1f));
+		break;
+
+	case ETimeFormat::SS_Ms:
+		Result = FString::Printf(TEXT("%02d.%d"), TotalSeconds, ConvertMilliseconds(0.01f));
+		break;
+
+	case ETimeFormat::SS:
+		Result = FString::Printf(TEXT("%02d"), TotalSeconds);
+		break;
+	}
+
+	return Result;
 }
--- Source/TrickyUserInterface/Public/TrickyUserInterfaceLibrary.h
+++ Source/TrickyUserInterface/Public/TrickyUserInterfaceLibrary.h
@@ -6,6 +6,17 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "TrickyUserInterfaceLibrary.generated.h"
 
+UENUM(BlueprintType)
+enum class ETimeFormat : uint8
+{
+	MM_SS_MsMs UMETA(DisplayName="MM:SS.MsMs"),
+	MM_SS_Ms UMETA(DisplayName="MM:SS.Ms"),
+	MM_SS UMETA(DisplayName="MM:SS"),
+	SS_MsMs UMETA(DisplayName="SS.MsMs"),
+	SS_Ms UMETA(DisplayName="SS.Ms"),
+	SS UMETA(DisplayName="SS")
+};
+
 /**
  * 
  */
@@ -15,12 +26,15 @@ class TRICKYUSERINTERFACE_API UTrickyUserInterfaceLibrary : public UBlueprintFun
 	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintCallable, Category="Settings")
-    	static void SetQualitySettings(const int32 QualityIndex);
-    
-    	UFUNCTION(BlueprintCallable, Category="Settings")
-    	static void SetScreenMode(EWindowMode::Type ScreenMode);
-    
-    	UFUNCTION(BlueprintCallable, Category="Settings")
-    	static void SetResolutionScale(const float Scale = 1.f);
+	UFUNCTION(BlueprintCallable, Category="TrickyUserInterface")
+	static void SetQualitySettings(const int32 QualityIndex);
+
+	UFUNCTION(BlueprintCallable, Category="TrickyUserInterface")
+	static void SetScreenMode(EWindowMode::Type ScreenMode);
+
+	UFUNCTION(BlueprintCallable, Category="TrickyUserInterface")
+	static void SetResolutionScale(const float Scale = 1.f);
+
+	UFUNCTION(BlueprintPure, Category="TrickyUserInterface")
+	static FString ConvertTimeSeconds(const float TimeSeconds, const ETimeFormat TimeFormat);
 };
