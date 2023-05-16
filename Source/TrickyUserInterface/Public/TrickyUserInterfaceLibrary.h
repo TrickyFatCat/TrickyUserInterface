571ec5229508896e249dfa191af24598c9ca9ceb
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-05-16 22:08:36 +0300

    Add comments to functions

--- Source/TrickyUserInterface/Public/TrickyUserInterfaceLibrary.h
+++ Source/TrickyUserInterface/Public/TrickyUserInterfaceLibrary.h
@@ -18,7 +18,7 @@ enum class ETimeFormat : uint8
 };
 
 /**
- * 
+ * A library which contain simple helper functions to adjust settings of the game and more
  */
 UCLASS()
 class TRICKYUSERINTERFACE_API UTrickyUserInterfaceLibrary : public UBlueprintFunctionLibrary
@@ -26,18 +26,23 @@ class TRICKYUSERINTERFACE_API UTrickyUserInterfaceLibrary : public UBlueprintFun
 	GENERATED_BODY()
 
 public:
+	/**Sets quality level from low (0) to epic (3)*/
 	UFUNCTION(BlueprintCallable, Category="TrickyUserInterface")
 	static void SetQualitySettings(const int32 QualityIndex);
 
+	/**Sets screen mode*/
 	UFUNCTION(BlueprintCallable, Category="TrickyUserInterface")
 	static void SetScreenMode(EWindowMode::Type ScreenMode);
 
+	/**Sets resolution scale*/
 	UFUNCTION(BlueprintCallable, Category="TrickyUserInterface")
 	static void SetResolutionScale(const float Scale = 1.f);
 
+	/**Sets screen resolution*/
 	UFUNCTION(BlueprintCallable, Category="TrickyUserInterface")
 	static void SetResolution(const int32 Width, const int32 Height);
-	
+
+	/**Converts given time in seconds to a chosen format*/
 	UFUNCTION(BlueprintPure, Category="TrickyUserInterface")
 	static FString ConvertTimeSeconds(const float TimeSeconds, const ETimeFormat TimeFormat);
 };
